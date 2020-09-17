0cbc8fcdd23e2e03b1d82dd99d0d746fdc25a849
Crowdbotics <team@crowdbotics.com>
2020-09-17 13:06:43 +0000

    add package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'BlankScreen4102898', human_name: 'Blank Screen 4', access_route: 'BlankScreen4102898'},
 { name: 'Messaging102897', human_name: 'Messaging', access_route: 'Messaging102897', icon: 'comments'},
 { name: 'CalendarView102896', human_name: 'Calendar View', access_route: 'CalendarView102896', icon: 'calendar'},
 { name: 'EmailAuth102895', human_name: 'Email Auth', access_route: 'EmailAuth102895', icon: 'envelope-o'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import BlankScreen4102898Navigator from '../features/BlankScreen4102898/navigator';
 import Messaging102897Navigator from '../features/Messaging102897/navigator';
 import CalendarView102896Navigator from '../features/CalendarView102896/navigator';
 import EmailAuth102895Navigator from '../features/EmailAuth102895/navigator';
@@ -16,6 +17,7 @@ import EmailAuth102895Navigator from '../features/EmailAuth102895/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+BlankScreen4102898: { screen: BlankScreen4102898Navigator },
 Messaging102897: { screen: Messaging102897Navigator },
 CalendarView102896: { screen: CalendarView102896Navigator },
 EmailAuth102895: { screen: EmailAuth102895Navigator },
