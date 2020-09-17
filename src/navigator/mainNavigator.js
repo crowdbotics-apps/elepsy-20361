e6ab55507fc9d680537d6d6fdc32bdbfc3d77c5b
Crowdbotics <team@crowdbotics.com>
2020-09-17 13:05:21 +0000

    add package blueprint-react-native-message-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Messaging102897Navigator from '../features/Messaging102897/navigator';
 import CalendarView102896Navigator from '../features/CalendarView102896/navigator';
 import EmailAuth102895Navigator from '../features/EmailAuth102895/navigator';
 
@@ -15,6 +16,7 @@ import EmailAuth102895Navigator from '../features/EmailAuth102895/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Messaging102897: { screen: Messaging102897Navigator },
 CalendarView102896: { screen: CalendarView102896Navigator },
 EmailAuth102895: { screen: EmailAuth102895Navigator },
 
