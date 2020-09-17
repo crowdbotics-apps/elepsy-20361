82f0b1e7ad2320154e7b1e7aa8ae7b76d7830dfb
Crowdbotics <team@crowdbotics.com>
2020-09-17 13:04:19 +0000

    Update models

--- backend/elepsy_20361/wsgi.py
+++ backend/elepsy_20361/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elepsy_20361.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elepsy_20361.settings")
 
 application = get_wsgi_application()
